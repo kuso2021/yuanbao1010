f4654f1bf46a9b61a95411994d98fcb37686d4f5
yuanbao <kuso_2021@qq.com>
2021-08-23 03:41:26 +0800

    version 1.0.0

--- src/main/java/com/xxxx/seckill/controller/UserController.java
+++ src/main/java/com/xxxx/seckill/controller/UserController.java
@@ -35,4 +35,22 @@ public class UserController {
     public void mq(){
         mqSender.send("hello");
     }
+
+    @ResponseBody
+    @RequestMapping("/mq/fanout")
+    public void mq01(){
+        mqSender.send("hello");
+    }
+
+    @ResponseBody
+    @RequestMapping("/mq/direct01")
+    public void mq02(){
+        mqSender.send01("hello,red");
+    }
+
+    @ResponseBody
+    @RequestMapping("/mq/direct02")
+    public void mq03(){
+        mqSender.send02("hello,green");
+    }
 }
--- src/main/java/com/xxxx/seckill/rabbitmq/MQReceiver.java
+++ src/main/java/com/xxxx/seckill/rabbitmq/MQReceiver.java
@@ -17,4 +17,24 @@ public class MQReceiver {
     public void receive(Object msg){
         log.info("接收消息："+msg);
     }
+
+    @RabbitListener(queues = "queue_fanout01")
+    public void receive01(Object msg){
+        log.info("QUEUE01接收消息："+msg);
+    }
+
+    @RabbitListener(queues = "queue_fanout02")
+    public void receive02(Object msg){
+        log.info("QUEUE02接收消息："+msg);
+    }
+
+    @RabbitListener(queues = "queue_direct01")
+    public void receive03(Object msg){
+        log.info("QUEUE01接收消息："+msg);
+    }
+
+    @RabbitListener(queues = "queue_direct02")
+    public void receive04(Object msg){
+        log.info("QUEUE02接收消息："+msg);
+    }
 }
